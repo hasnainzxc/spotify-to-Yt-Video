1878bd90065e76e37c1586a6a848a4cabff98aa4
Hasnian <52049324+hasnainzxc@users.noreply.github.com>
2025-06-29 11:09:06 +0500

    Delete portfolio-v3-main directory

--- portfolio-v3-main/data/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/about', title: 'About' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/contact', title: 'Contact' },
-];
-
-export default headerNavLinks;
